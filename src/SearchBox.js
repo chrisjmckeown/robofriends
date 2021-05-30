89cf32bef742f76c96ee646c063b8b790b664116
Chris Mckeown <chris.j.mckeown@hotmail.com>
2021-05-30 14:37:05 +1000

    app created

--- src/SearchBox.js
+++ src/SearchBox.js
@@ -1,12 +1,13 @@
 import React from 'react';
 
-export const SearchBox = () => {
+export const SearchBox = ({ searchChange, searchField }) => {
   return (
     <div className='pa2'>
       <input
         className='pa3 ba b--green bg-lightest-blue'
         type='search'
         placeholder='search robots'
+        onChange={searchChange}
       ></input>
     </div>
   );
